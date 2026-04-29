d3334689516be6aa37773a15f00116d9397bdff9
JITANSHU-17 <jitanshupatra06@gmail.com>
2026-04-29 10:57:29 +0530

    Update Db.js
    CHANGE THE .ENV .

--- src/utils/Db.js
+++ src/utils/Db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDb=async()=>{
     
-    await mongoose.connect(process.env.DB_STRING);
+    await mongoose.connect(process.env.MONGO_URI);
        
 }
