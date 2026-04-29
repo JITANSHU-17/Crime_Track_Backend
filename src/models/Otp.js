53d435c246b9b1527b4c76c719087154ff63f782
JITANSHU-17 <jitanshupatra06@gmail.com>
2026-04-29 11:31:52 +0530

    Create Otp.js
    Otp.js is created .

---
+++ src/models/Otp.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, expires: 300 } // Auto-delete after 5 mins (300 seconds)
+});
+
+export const Otp = mongoose.model("Otp", otpSchema);
