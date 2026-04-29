e5136fd501e55acc0eaca35434e6a1bd99cd845a
JITANSHU-17 <jitanshupatra06@gmail.com>
2026-04-29 10:41:00 +0530

    Update app.js
    Same error

--- src/app.js
+++ src/app.js
@@ -1,33 +1,34 @@
 import express from "express";
-import cors from "cors";
+import cors from "cors"
 import dotenv from "dotenv";
 import { connectDb } from "./utils/Db.js";
 import { userroute } from "./routes/User.route.js";
-
 dotenv.config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
-app.get("/", (req, res) => {
+
+connectDb()
+.then(()=>{
+  console.log("db connected successfully")
+  app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-app.use("/api/auth", userroute);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-connectDb()
-  .then(() => {
-    console.log("db connected successfully");
+})
+.catch(()=>{
+  console.log("issue in db connection")
+})
+ 
+app.use("/api/auth",userroute)
 
-    app.listen(port, "0.0.0.0", () => {
-      console.log(`Server running on port ${port}`);
-    });
-  })
-  .catch(() => {
-    console.log("issue in db connection");
-  });
 
 export default app;
